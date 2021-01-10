f4ec1483ba9c12b49b557aa291f30c2111edaaea
kmb <mcdba@mail.ru>
2021-01-10 22:26:58 +0300

    getFetch with cache

---
+++ src/routes/catalog/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Spinner from "./Spinner.svelte";
+	import { getData } from "./fetcher";
+	const response = getData("");
+</script>
+
+<h1>Modern Fetch</h1>
+{#await $response}
+	<Spinner />
+{:then data}
+	<hr />
+	<p>{JSON.stringify(data, null, 2)}</p>
+{:catch}
+	<p>Err</p>
+{/await}
